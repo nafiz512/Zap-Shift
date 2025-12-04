293f7eb2d60a9776ee7a7d8cd618f18178a9c55d
nafiz512 <nafizmuntasir512@gmail.com>
2025-12-04 18:59:22 +0600

    added loayout and routes

---
+++ src/routes/router.jsx
@@ -0,0 +1,97 @@
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../layouts/RootLayout";
+import Home from "../pages/Home/Home/Home";
+import Coverage from "../pages/Coverage/Coverage";
+import AuthLayout from "../layouts/AuthLayout";
+import Login from "../pages/Auth/Login/Login";
+import Register from "../pages/Auth/Register/Register";
+import PrivateRoute from "./PrivateRoute";
+import Rider from "../pages/Rider/Rider";
+import SendParcel from "../pages/sendParcel/SendParcel";
+import DashboardLayout from "../layouts/DashboardLayout";
+import MyParcels from "../pages/Dashboard/MyParcels/MyParcels";
+import Payment from "../pages/Dashboard/Payment/Payment";
+import PaymentSuccess from "../pages/Dashboard/Payment/PaymentSuccess";
+import PaymentCancelled from "../pages/Dashboard/Payment/PaymentCancelled";
+import PaymentHistory from "../pages/Dashboard/PaymentHistory/PaymentHistory";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        Component: RootLayout,
+        children: [
+            {
+                index: true,
+                Component: Home,
+            },
+            {
+                path: "rider",
+                element: (
+                    <PrivateRoute>
+                        <Rider></Rider>
+                    </PrivateRoute>
+                ),
+            },
+            {
+                path: "send-parcel",
+                element: (
+                    <PrivateRoute>
+                        <SendParcel></SendParcel>
+                    </PrivateRoute>
+                ),
+                loader: () =>
+                    fetch("/serviceCenters.json").then((res) => res.json()),
+            },
+            {
+                path: "coverage",
+                Component: Coverage,
+                loader: () =>
+                    fetch("/serviceCenters.json").then((res) => res.json()),
+            },
+        ],
+    },
+    {
+        path: "/",
+        Component: AuthLayout,
+        children: [
+            {
+                path: "login",
+                Component: Login,
+            },
+            {
+                path: "register",
+                Component: Register,
+            },
+        ],
+    },
+    {
+        path: "dashboard",
+        element: (
+            <PrivateRoute>
+                <DashboardLayout></DashboardLayout>
+            </PrivateRoute>
+        ),
+        children: [
+            {
+                path: "my-parcels",
+                Component: MyParcels,
+            },
+            {
+                path: "payment/:parcelId",
+                Component: Payment,
+            },
+            {
+                path: "payment-success",
+                Component: PaymentSuccess,
+            },
+            {
+                path: "payment-cancelled",
+                Component: PaymentCancelled,
+            },
+            {
+                path: "payment-history",
+                Component: PaymentHistory,
+            },
+        ],
+    },
+]);
