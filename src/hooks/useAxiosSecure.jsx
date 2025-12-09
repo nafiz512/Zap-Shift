c0f48847e8a771e4df569ef2c9c5a41639329354
nafiz512 <nafizmuntasir512@gmail.com>
2025-12-09 21:12:28 +0600

    added hooks

---
+++ src/hooks/useAxiosSecure.jsx
@@ -0,0 +1,51 @@
+import axios from "axios";
+import React from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
+
+const axiosSecure = axios.create({
+    baseURL: "http://192.168.1.104:3000",
+});
+
+const useAxiosSecure = () => {
+    const { user, logOut } = useAuth();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        // intercept request
+        const reqInterceptor = axiosSecure.interceptors.request.use(
+            (config) => {
+                config.headers.Authorization = `Bearer ${user?.accessToken}`;
+                return config;
+            }
+        );
+
+        // interceptor response
+        const resInterceptor = axiosSecure.interceptors.response.use(
+            (response) => {
+                return response;
+            },
+            (error) => {
+                console.log(error);
+
+                const statusCode = error.status;
+                if (statusCode === 401 || statusCode === 403) {
+                    logOut().then(() => {
+                        navigate("/login");
+                    });
+                }
+
+                return Promise.reject(error);
+            }
+        );
+
+        return () => {
+            axiosSecure.interceptors.request.eject(reqInterceptor);
+            axiosSecure.interceptors.response.eject(resInterceptor);
+        };
+    }, [user, logOut, navigate]);
+    return axiosSecure;
+};
+
+export default useAxiosSecure;
